9f771db04653a069b024c53401c09d229b4ff2b8
sofia durkan <sofiadurkan@gmail.com>
2021-06-24 14:48:32 -0500

    question updated.

--- app.js
+++ app.js
@@ -50,6 +50,9 @@ console.log(getFirstValue([1,2,3]));
 //Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
 
 function addUp(num) {
+    if (num === 1){
+        return 1;
+    }
     return num + addUp(num-1);
 	
 }
