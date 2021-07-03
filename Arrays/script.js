020c80f640989ff06b7fa2866b0e977c3d57b8bb
sofia durkan <sofiadurkan@gmail.com>
2021-07-03 10:44:41 -0500

    first commit

---
+++ Arrays/script.js
@@ -0,0 +1,71 @@
+let bests = ["Clarusway","Google","Tesla","Amazon",555,777];
+// console.log(bests);
+// console.log(bests[1]);
+// console.log(bests[bests.length-1]);
+
+
+// let newVariable = bests[0];
+// console.log("newVariable",newVariable,"type of new variable is:",typeof newVariable,"");
+
+// let newVariable = bests[bests.length-1];
+
+
+
+// bests[2]= "SpaceX";
+// bests[11]="test";
+// bests[2]=111;
+// console.log(bests);
+// console.log("here is the new array:",bests);
+// console.log("here is the new array:"+ bests);
+
+// console.log("here is the number",222);
+// console.log("here is the number " + 222);
+
+// const lengthOfArray =bests.length
+
+// console.log("bests.length: ", bests.length);
+
+
+//CONCAT
+
+// let dogs =["bulldog","beagle","rottweiler"];
+// let cats = ["ragdoll","sphynx","birman"];
+// let birds = ["parrot","budgerigar","cockatoos"]
+
+// // console.log(dogs.concat(cats));
+// console.log(dogs.concat(dogs,cats,birds));
+// console.log(dogs.concat(dogs,[11,22,33],cats,["www",4444]));
+
+
+// //SORT
+// const names = ['John', 'James', 'Walter', 'Edward'];
+
+// console.log(names.sort()); //method
+// console.log(names.length); //this is not a method
+
+// const numbers = [45,16,78,8,25,5,99,1006];
+// console.log(numbers.sort());
+
+//PUSH
+// const names = ['John', 'James', 'Walter', 'Edward'];
+// names.push("Javid");
+
+// console.log(names);
+
+
+//POP
+
+// let poppedEl = names.pop();
+// console.log(names);
+// console.log("poppedElement",poppedEl);
+
+//SHIFT  UNSHIFT
+const names = ['John', 'James', 'Walter', 'Edward'];
+console.log(names.shift()); //first element deletes.
+
+let shiftedEl = names.shift();
+console.log(shiftedEL);
+
+
+
+
