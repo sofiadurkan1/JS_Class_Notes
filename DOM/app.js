9197bf5c4314c3c34e1f26e37f1abacf12e9c3dd
sofia durkan <sofiadurkan@gmail.com>
2021-07-11 14:30:26 -0500

    first commit

---
+++ DOM/app.js
@@ -0,0 +1,23 @@
+// document get element by id
+
+const myDom = document.getElementById("dom");
+console.log(myDom);
+
+// const instructor = document.getElementById("info");
+// console.log(instructor);
+
+
+const instructor = document.getElementById("instructor");
+console.log(instructor);
+
+//classname
+
+const withClassName = document.getElementsByClassName("class-info")
+console.log(withClassName[0]);
+
+const cw = document.getElementsByTagName("h3");
+console.log(cw);
+
+
+//query selector
+
