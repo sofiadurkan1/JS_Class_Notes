6c60c60f3561f01f89ecec1138ac24c8a92212db
sofia durkan <sofiadurkan@gmail.com>
2021-07-28 19:59:10 -0500

    first commit

---
+++ DOM_RECAP/app.js
@@ -0,0 +1,109 @@
+// //ID
+
+// const myFirstElement = document.getElementById('userName');
+// console.log("myFirstElement");
+
+// //CLASS
+
+// const myClass = document.getElementsByClassName('userTag');
+// console.log(myClass);
+
+// //TAG
+
+// const myTag = document.getElementsByTagName('p');
+// console.log(myTag);
+
+
+// //ID
+
+// const elementByQuery = document.querySelector('#userName');
+// console.log(elementByQuery)
+
+// //query selector class
+
+// const elementByQuery2 = document.querySelector('.userTag');
+// console.log(elementByQuery2)
+
+// //query selectorAll class
+
+// const elementByQuery3 = document.querySelectorAll('.userTag');
+// console.log(elementByQuery3)
+
+
+// const elementByQuery4 = document.querySelectorAll('p');
+// console.log(elementByQuery4)
+
+//------------------------Second--------------------------
+// const langList = document.querySelector("#languages");
+// console.log(langList);
+
+
+// const langsGetElem = document.getElementsByClassName("lang");
+// const langsQerySe = document.querySelector(".lang");
+
+// console.log(langsGetElem);
+// console.log(langsQerySe);
+
+// langList.innerHTML += `<li class="lang">JavaScript</li>`
+
+// console.log(langsGetElem);
+// console.log(langsQerySe);
+
+
+// const element = document.querySelector("#myElement");
+// console.log(element);
+
+
+// element.innerHTML = "Hello world..";
+// element.innerText = "Hello";
+
+
+
+
+//-----------------------------------------
+
+
+// const car_list = [
+//     { brandName: "Mercedes"},
+//     { brandName: "BMW"},
+//     { brandName: "Toyota"},
+//     { brandName: "Opel"},
+// ];
+
+// const listElement = document.querySelector('#cars');
+// console.log(listElement)
+
+// for(let index = 0 ; index < car_list.length;index ++){
+//     listElement.innerHTML += `<li>${car_list[index].brandName}</li>`
+// }
+
+
+
+//for each
+
+// car_list.forEach(car =>{
+//     console.log(car)
+//     listElement.innerHTML += `<li>${car.brandName}</li>`
+
+//     // listElement.innerHTML += '<li>' + car_list[i].brandName + '</li>';
+
+// })
+
+//-------------------------------------------
+
+const car_list = [
+    { id:0, brandName: "Mercedes"},
+    { id:1, brandName: "BMW"},
+    { id:2, brandName: "Toyota"},
+    { id:3, brandName: "Opel"},
+];
+
+
+const comboBox =document.createElement("select");
+
+car_list.forEach(car =>{
+    comboBox.innerHTML +=`<option value="${car.id}">${car.brandName}</option>`
+})
+
+document.body.append(comboBox);
+
